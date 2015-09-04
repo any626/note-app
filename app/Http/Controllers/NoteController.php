36faa58368567d3461e20a2bd84c2da29b969c7a
Alejandro Landeros <theyyys@gmail.com>
2015-09-03 18:26:57 -0700

    create controllers and removed laravel defaults

---
+++ app/Http/Controllers/NoteController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class NoteController extends Controller
+{
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    public function get(){
+        //
+    }
+
+    public function edit($id){
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function delete($id)
+    {
+        //
+    }
+}
